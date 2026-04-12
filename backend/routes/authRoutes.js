759df7cf498cded9ffc8e2050566a4dc641e4f99
juicychuu <manzanjenny30@gmail.com>
2026-04-12 20:55:27 +0800

    routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const {body, validationResult} = require('express-validator');
+const { body, validationResult } = require('express-validator');
 const rateLimit = require('express-rate-limit');
 
 const authLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 10,
-    message:{ error: 'Too many authentication attempts, please try again after 15 minutes.'}
+    message: { error: 'Too many authentication attempts, please try again after 15 minutes.' }
 });
 
 const validate = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array()[0].msg});
+        return res.status(400).json({ error: errors.array()[0].msg });
     }
     next();
 };
 
 router.post('/register', authLimiter, [
-    body('username').trim().notEmpty().withMessage('Username is required').isLength({min:3}).withMessage('Username must be atleast 3 characters'),
-    body('email').isEmail().withMessage('Please provide a valide email'),
-    body('password').isLength({min:6}).withMessage('Password must be atleast 6 characters')
+    body('username').trim().notEmpty().withMessage('Username is required').isLength({ min: 3 }).withMessage('Username must be at least 3 characters'),
+    body('email').isEmail().withMessage('Please provide a valid email'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters')
 ], validate, authController.register);
 
 router.post('/login', authLimiter, [
@@ -32,5 +32,3 @@ router.post('/login', authLimiter, [
 router.post('/logout', authController.logout);
 
 module.exports = router;
-
-    
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const productController = require('../controllers/productController');
 const verifyToken = require('../middleware/authMiddleware');
 const isAdmin = require('../middleware/roleMiddleware');
+const upload = require('../middleware/uploadMiddleware');
 
 router.get('/', productController.getAllProducts);
-router.post('/', verifyToken, isAdmin, productController.createProduct);
 router.post('/', verifyToken, isAdmin, upload.single('image'), productController.createProduct);
+
+module.exports = router;
